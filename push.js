6ce737a2d61f9016bfe23544f7abeb6da3372ae6
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-19 17:08:20 +0600

    testing es6

---
+++ push.js
@@ -0,0 +1,12 @@
+
+let arr = [29, 31, 39, 41];
+arr.push(49);
+
+arr[2] = 50;
+
+console.log(arr);
+const sum = 0;
+arr.forEach(arrs =>{
+    const total = sum + arrs 
+    console.log(total);
+})
---
+++ test.js
@@ -0,0 +1,6 @@
+
+
+// find
+const test = [ 20, 40, 10, 30, 18];
+const result = test.filter(res=> res >= 20);
+console.log(result);
